49130739ea44f947d8680025b97bdd76188727d0
Manuel Garcia-Nieto <mgarcianieto@gmail.com>
2023-02-17 22:08:57 +0100

    Add images and data.json

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 import express from 'express'
+import data from './data.json'
 
 const app = express()
+
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+  res.send('Its no matter')
+})
 
 const PORT = 3000
 
 app.listen(PORT, () => {
-  console.log(`The server is running on Port: ${PORT}!`); 
+  console.log(`The server is running on 127.0.0.1:${PORT}`)
 })
